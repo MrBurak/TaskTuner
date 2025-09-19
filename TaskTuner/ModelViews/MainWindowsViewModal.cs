6ea6d301b13132103b5b18728562c0fbcefb2add
MrBurak <bkepti@gmail.com>
2025-09-19 12:28:14 +0300

    NewTask Window Added

--- TaskTuner/ModelViews/MainWindowsViewModal.cs
+++ TaskTuner/ModelViews/MainWindowsViewModal.cs
@@ -4,12 +4,26 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using TaskTuner.Views;
 
 namespace TaskTuner.ModelViews
 {
     public class MainWindowsViewModal : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ICommand IOpenNewWindow => new RelayCommand(OpenNewWindow, null!);
+
+        private void OpenNewWindow() 
+        {
+            NewTaskWindow newTaskWindow = new NewTaskWindow();
+            newTaskWindow.Show();
+        }
+
+        
+
+
 
         protected virtual void OnPropertyChanged(string propertyName) 
         {
